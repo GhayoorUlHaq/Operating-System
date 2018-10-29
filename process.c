07406e7a5c600efa54f2bda5d662b7c4fd661ecd
Ghayoor Ul Haq <ghayoor10th@gmail.com>
2018-10-29 22:42:58 +0500

    10 process program updated

--- process.c
+++ process.c
@@ -4,179 +4,55 @@
 #include <stdlib.h>
 pid_t fork(void);
 
-int solve(int i, int e, const int arr[]) {
-    int s = 0;
-    for (int j = i; j < e; j++) {
-        s = s + arr[j];
+#define processes 10
+int solve(int start, const int arr[]) {
+    int sum = 0;
+    for (int i = start * 100; i < (start * 100) + 100; ++i) {
+        sum = sum + arr[i];
     }
-    return s;
+    return sum;
 }
 int main() {
-    int s;
+    int temp;
     int sum;
     int arr[1000];
-    for (int i = 0; i < 1000; ++i) {
+
+    for (int i = 0; i < 1000; i++) {
         arr[i] = i + 1;
     }
 
-    int p1[2];
-    pipe(p1);
-    int p2[2];
-    pipe(p2);
-    int p3[2];
-    pipe(p3);
-    int p4[2];
-    pipe(p4);
-    int p5[2];
-    pipe(p5);
-    int p6[2];
-    pipe(p6);
-    int p7[2];
-    pipe(p7);
-    int p8[2];
-    pipe(p8);
-    int p9[2];
-    pipe(p9);
-    int p10[2];
-    pipe(p10);
-
+    int pipes[10][2];
+    for (int k = 0; k < processes; k++) {
+        pipe(pipes[k]);
+    }
 
     int pid = fork();
-    if (pid == 0) {
-        s = solve(0, 100, arr);
-        write(p10[1], &s, sizeof(s));
-        close(p10[1]);
-        exit(1);
-    } else {
+
+    for (int j = 0; j < processes ; j++) {
         pid = fork();
         if (pid == 0) {
-
-            s = solve(100, 200, arr);
-            write(p1[1], &s, sizeof(s));
-            close(p1[1]);
+            temp = solve(j,  arr);
+            write(pipes[j][1], &temp, sizeof(temp));
+            close(pipes[j][1]);
             exit(1);
-        } else {
-            pid = fork();
-            if (pid == 0) {
-
-                s = solve(200, 300, arr);
-                write(p2[1], &s, sizeof(s));
-                close(p2[1]);
-                exit(1);
-            } else {
-                pid = fork();
-                if (pid == 0) {
-
-                    s = solve(300, 400, arr);
-                    write(p3[1], &s, sizeof(s));
-                    close(p3[1]);
-                    exit(1);
-                } else {
-                    pid = fork();
-                    if (pid == 0) {
-
-                        s = solve(400, 500, arr);
-                        write(p4[1], &s, sizeof(s));
-                        close(p4[1]);
-                        exit(1);
-                    } else {
-                        pid = fork();
-                        if (pid == 0) {
-
-                            s = solve(500, 600, arr);
-                            write(p5[1], &s, sizeof(s));
-                            close(p5[1]);
-                            exit(1);
-                        } else {
-                            pid = fork();
-                            if (pid == 0) {
-
-                                s = solve(600, 700, arr);
-                                write(p6[1], &s, sizeof(s));
-                                close(p6[1]);
-                                exit(1);
-                            } else {
-                                pid = fork();
-                                if (pid == 0) {
-
-                                    s = solve(700, 800, arr);
-                                    write(p7[1], &s, sizeof(s));
-                                    close(p7[1]);
-                                    exit(1);
-                                } else {
-                                    pid = fork();
-                                    if (pid == 0) {
-                                        s = solve(800, 900, arr);
-                                        write(p8[1], &s, sizeof(s));
-                                        close(p8[1]);
-                                        exit(1);
-                                    } else {
-                                        pid = fork();
-                                        if (pid == 0) {
-                                            s = solve(900, 1000, arr);
-                                            write(p9[1], &s, sizeof(s));
-                                            close(p9[1]);
-                                            exit(1);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
-
     //Wait until all processes got processed
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < processes; i++) {
         wait(NULL);
     }
-
     //Reading data from pipe and add to "sum"
-    int sm = 0;
-    read(p1[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p2[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p3[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p4[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p5[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p6[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p7[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p8[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p9[0], &sm, sizeof(sm));
-    sum = sum + sm;
-    sm = 0;
-    read(p10[0], &sm, sizeof(sm));
-    sum = sum + sm;
+    for (int l = 0; l < processes ; l++) {
+        int tempsum = 0;
+        read(pipes[l][0], &tempsum, sizeof(tempsum));
+        sum = sum + tempsum;
+    }
 
-    close(p1[0]);
-    close(p2[0]);
-    close(p3[0]);
-    close(p4[0]);
-    close(p5[0]);
-    close(p6[0]);
-    close(p7[0]);
-    close(p8[0]);
-    close(p9[0]);
-    close(p10[0]);
+    //closing all pipes
+    for (int m = 0; m < processes; m++) {
+        close(pipes[m][0]);
+    }
 
 
     printf("Sum of 0-1000: %d\n", sum);
-
 }
